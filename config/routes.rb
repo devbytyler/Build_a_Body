6e10b02bc97eaeb9000bcdfac70842c208a8454a
tylerstephens814 <tylerstephens814@gmail.com>
2017-03-23 15:41:09 -0600

    Add about page from static controller with corresponding route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root to: 'static#home'
+  
+  get 'about' , to: "static#about"
 
   resources :exercises
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
